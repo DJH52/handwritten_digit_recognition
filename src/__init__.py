4924f2ef267ddfddca2337a5e32eac8e3771b7a8
DJH52 <37420877+DJH52@users.noreply.github.com>
2023-08-27 17:10:48 +0100

    Comments
    added the general format for code descriptions.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,15 @@
+"""
+File: __init__.py
+Description: This file is used to make the src folder a python package.
+
+Author: Daniel Holmes
+Date: 27/08/2023
+
+"""
 def main():
-    print("welcome to the program")
+    """ This function is used to test the package. """
+
+    print("Hello world!")
 
 
 if __name__ == "__main__":
--- src/models/neural_network.py
+++ src/models/neural_network.py
@@ -7,3 +7,24 @@ Date: 27/08/2023
 
 """
 
+class NeuralNetwork:
+    """ This class blueprint is for a neural network model. """
+
+    # Class attributes
+    def __init__(self):
+        """ This function is used to initialise the class. """
+        self.num_layers = 0
+        self.layers_size = []
+        self.layers = []
+        self.weights = []
+        self.biases = []
+        self.activations = []
+    
+        pass
+    
+    # Class methods
+    def add_layer(self, layer_size, activation):
+        """ This function is used to add a layer to the model. """
+        pass
+
+    
